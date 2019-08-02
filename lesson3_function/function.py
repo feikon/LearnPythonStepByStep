4ecec66c20891424a9b8e99c846c8d57a036fe97
feikon <hpuyanfeng@126.com>
2019-08-02 22:18:15 +0800

    add lesson3

---
+++ lesson3_function/function.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on 八月-02-19 22:10
+# function.py
+# @author: crossfirestater@gmail.com
+'''
+
+def add2Num(num1, num2):
+    num3 = num1 + num2
+    print(num3)
+    return num3
+
+
+
+def test_add2Num():
+    assert add2Num(1, 1) == 2
+    assert add2Num(1, 2) == 3
+
+if __name__ == "__main__":
+    add2Num(121, 546313)
